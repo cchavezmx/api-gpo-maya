9f5f1eee47c68fdfeae4140e74c1e833da9af8ca
Carlos A. Chávez Rosario <66329126+cchavezmx@users.noreply.github.com>
2021-08-25 11:49:30 -0500

    cambio de url de mongo

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const NODE_ENV = process.env.NODE_ENV
 
 const config = {
   test: {
-    MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@gti-shard-00-00.tcrun.mongodb.net:27017,gti-shard-00-01.tcrun.mongodb.net:27017,gti-shard-00-02.tcrun.mongodb.net:27017/gpoMayaDev?ssl=true&replicaSet=atlas-tyqnx6-shard-0&authSource=admin&retryWrites=true&w=majority`
+    MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00.75eiz.mongodb.net:27017,cluster0-shard-00-01.75eiz.mongodb.net:27017,cluster0-shard-00-02.75eiz.mongodb.net:27017/test?ssl=true&replicaSet=atlas-u78g7x-shard-0&authSource=admin&retryWrites=true&w=majority`
   }
 
 }
